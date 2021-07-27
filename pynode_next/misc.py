1d2f2fb6fa77e3c81d8c2d865bdfe774a01caf53
Darcy LF <ehnevault@gmail.com>
2021-07-27 21:56:06 +1000

    update greys

--- pynode_next/misc.py
+++ pynode_next/misc.py
@@ -17,9 +17,9 @@ Color.GREEN = Color(0, 150, 0)
 Color.BLUE = Color(0, 0, 200)
 Color.YELLOW = Color(255, 215, 0)
 Color.WHITE = Color(255, 255, 255)
-Color.LIGHT_GREY = Color(199, 199, 199)
-Color.GREY = Color(127, 127, 127)
-Color.DARK_GREY = Color(82, 82, 82)
+Color.LIGHT_GREY = Color(179, 179, 179)
+Color.GREY = Color(102, 102, 102)
+Color.DARK_GREY = Color(26, 26, 26)
 Color.BLACK = Color(0, 0, 0)
 
 def pause(pause_time):
