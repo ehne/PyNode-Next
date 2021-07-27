3ce86b5aba03486d791e6cd8d29f2e8a8aae5f36
Darcy LF <ehnevault@gmail.com>
2021-07-27 17:39:55 +1000

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,12 @@ from pynode_next import *
 
 def test():
     graph.add_node("a", value="huh")
-    
-    pause(1000)
-    
-    print(graph.node("a"))
+        
+    graph.add_node("b")
 
     graph.node("a").set_value("hi")
     print(graph.nodes())
-    graph.add_node("a")
+    
 
 
 begin_pynode_next(test)
