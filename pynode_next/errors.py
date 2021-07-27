b345ac77fc41d340e01578f4b5e121f590e553c2
Darcy LF <ehnevault@gmail.com>
2021-07-27 18:24:34 +1000

    add errors for edges

--- pynode_next/errors.py
+++ pynode_next/errors.py
@@ -3,10 +3,17 @@ from .core import core
 class Error(Exception):
     """The base error exception."""
     def __init__(self, message):
-        print("send error to ax")
         # self.canvas.dispatch(dispatch_dict)
         core.ax(lambda x: x.dispatch({"isPyNodeNext": True, "type": "error", "message": message}))
 
 class DuplicateNodeError(Error):
     """Raised when a node with the same id as a previous one was added."""
+    pass
+
+class DuplicateEdgeError(Error):
+    """Raised when a edge with the exact same values has been added to the graph."""
+    pass
+
+class NodeDoesntExistError(Error):
+    """Raised when a node does not exist in the graph"""
     pass
