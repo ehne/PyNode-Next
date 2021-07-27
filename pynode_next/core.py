5c83e7eff1b578ebadc6403b1f873e36e5d0ad4e
Darcy LF <ehnevault@gmail.com>
2021-07-27 14:16:31 +1000

    move core into core.py

---
+++ pynode_next/core.py
@@ -0,0 +1,28 @@
+import algorithmx as algx
+from pathlib import Path
+import os
+
+
+class Core:
+    def __init__(self, port=5050):
+        self.port = 5050
+        base_path = os.path.relpath(__file__)
+        custom_ui = f"{Path(base_path).parent}/ui.html"
+
+        print("serving ui found at", custom_ui)
+        self.server = algx.http_server(port=port, file=custom_ui)
+
+        self.canvas = self.server.canvas()
+
+    def run(self, func):
+        """A decorator that runs a function in the PyNode Next web environment."""
+        self.canvas.onmessage("start", func)
+        print(f"staring server on http://localhost:{self.port} — press ctrl+c to quit")
+        self.server.start()
+
+    def ax(self, func):
+        """A function that does all of the interfacing with AlgorithmX"""
+        func(self.canvas)
+
+
+core = Core()
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 from pynode_next import *
 
+def test():
+    print(core)
+    core.ax(lambda a: a.node(1).add())
+
+begin_pynode_next(test)
