06705d83b75221f0267a9ea52b937ed2c76bd26a
Darcy LF <ehnevault@gmail.com>
2021-07-27 14:29:50 +1000

    init Graph class

--- pynode_next/__init__.py
+++ pynode_next/__init__.py
@@ -6,6 +6,12 @@ from .core import core
 class Edge:
     pass
  
+class Graph:
+    def __init__(self):
+        self._nodes = {}
+        self._edges = []
+
+graph = Graph()
 
 def begin_pynode_next(func):
     core.run(func)
