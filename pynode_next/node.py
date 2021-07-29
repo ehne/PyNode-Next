dcc0da0147d0604885a687e1abc362c6cd526add
Darcy LF <ehnevault@gmail.com>
2021-07-29 12:32:50 +1000

    Update node.py

--- pynode_next/node.py
+++ pynode_next/node.py
@@ -3,13 +3,14 @@ import uuid
 from .misc import Color
 from .core import core
 
+
 class Node:
     def __init__(self, id=None, value=None):
         if id != None:
             self._id = id
         else:
             self._id = str(uuid.uuid4())
-        
+
         if value != None:
             self._value = value
         else:
@@ -17,11 +18,11 @@ class Node:
 
         self._incident_edges = []
         self._color = Color.DARK_GREY
-        
+
         self._attrs = {}
 
         self._labels = {}
-    
+
     def id(self):
         """Returns the node's id"""
         return self._id
@@ -51,27 +52,26 @@ class Node:
     def attribute(self, name):
         """Returns a nodes named attribute"""
         return self._attrs[name]
-    
+
     def set_label(self, value, label_id=0):
         """Sets a node's label. 0 for top right and 1 for top left."""
         self._labels[0] = value
         label = ["tr", "tl"][label_id]
-        label_angle = [45, 45+90][label_id]
-        
-        core.ax(lambda x: x.dispatch({
-            "attrs": {
-                "nodes": {
-                    self._id: {
-                        "labels": {
-                            label: {
-                                "angle": label_angle,
-                                "text": value
+        label_angle = [45, 45 + 90][label_id]
+
+        core.ax(
+            lambda x: x.dispatch(
+                {
+                    "attrs": {
+                        "nodes": {
+                            self._id: {
+                                "labels": {label: {"angle": label_angle, "text": value}}
                             }
                         }
                     }
                 }
-            }
-        }))
+            )
+        )
 
     def label(self, label_id=0):
         return self._labels[label_id]
@@ -79,7 +79,7 @@ class Node:
     def incident_edges(self):
         """Returns the incident edges through the node."""
         return list(self._incident_edges)
-    
+
     def incoming_edges(self):
         """Returns the edges going into the node"""
         return [e for e in self._incident_edges if not e._directed or e._target == self]
@@ -91,12 +91,12 @@ class Node:
     def degree(self):
         """Returns the degree of the node."""
         return len(self._incident_edges)
-    
-    def indegree(self): 
+
+    def indegree(self):
         """Returns the number of edges going into the node."""
         return len(self.incoming_edges())
-        
-    def outdegree(self): 
+
+    def outdegree(self):
         """Returns the number of edges going out of the node."""
         return len(self.outgoing_edges())
 
@@ -110,15 +110,12 @@ class Node:
                 "nodes": {
                     self._id: {
                         "color": str(self._color),
-                        "labels": {
-                            0: {
-                                "text": self._value
-                            }
-                        }
+                        "labels": {0: {"text": self._value}},
                     }
                 }
             }
         }
+
 
 if __name__ == "__main__":
     assert Node(id="cool")._value == "cool"
