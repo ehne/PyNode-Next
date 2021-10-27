ehne/PyNode-Next

commit 5d62da4a6f4c754941c439ba50bc30654217ac40

:bug: make data method represent Node state better
#19

pynode_next/node.py
@@ -210,7 +210,12 @@ class Node:
                     self._id: {
                         "color": str(self._color),
                         "labels": {0: {"text": self._value}},
-                        "listenclick": True
+                        "listenclick": True,
+                        "size": self._size,
+                        'labels': {
+                            'tr': { 'angle': 45, 'text': self._labels[0] },
+                            'tl': { 'angle': 135, 'text': self._labels[1] }
+                        }
                     }
                 }
             }

test.py
@@ -8,9 +8,7 @@ import random
 
 def test():
     graph.add_node("a")
-    graph.add_node("b")
-    pause(500)
-    graph.add_edge("a", "b")
-    register_click_handler(lambda x: x.set_color(Color.GREEN))
+    graph.add_node("b").set_size(50)
+    graph.node('b').set_label('top right').set_label('top left', 1)
     
 begin_pynode_next(test)

commit 4771d04a22f4599c23afa2286bc7b19d0fc323da

:bug: fix issue caused by 5d62da4

pynode_next/node.py
@@ -211,15 +211,17 @@ class Node:
                         "color": str(self._color),
                         "labels": {0: {"text": self._value}},
                         "listenclick": True,
-                        "size": self._size,
-                        'labels': {
-                            'tr': { 'angle': 45, 'text': self._labels[0] },
-                            'tl': { 'angle': 135, 'text': self._labels[1] }
-                        }
+                        "size": self._size
                     }
                 }
             }
         }
+
+        if 0 in self._labels.keys():
+            base['attrs']['nodes'][self._id]['labels']['tr'] = { 'angle': 45, 'text': self._labels[0] }
+        if 1 in self._labels.keys():
+            base['attrs']['nodes'][self._id]['labels']['tl'] = { 'angle': 135, 'text': self._labels[1] }
+
         if self._pos != []:
             base["attrs"]["nodes"][self._id]["pos"] = [core.normalise_to_canvas(i) for i in self._pos]
 

test.py
@@ -7,8 +7,6 @@ import random
 
 
 def test():
-    graph.add_node("a")
-    graph.add_node("b").set_size(50)
-    graph.node('b').set_label('top right').set_label('top left', 1)
+    graph.add_node("a").set_value('1')
     
 begin_pynode_next(test)