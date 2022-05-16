45b353a307d1c62b766e00115774bc683c4fd136
Darcy LF <ehnevault@gmail.com>
2022-05-16 20:47:18 +1000

    :alembic: test bug fix

--- test.py
+++ test.py
@@ -8,5 +8,9 @@ from pynode_next import *
 def test():
     a = graph.add_node('a')
     a.set_color(Color.from_hex_int(0x0055ff))
+    graph.add_node('b')
+    graph.add_edge('a', 'b').set_width(40)
 
+    print(graph.has_node(a))
+    print(graph.has_node(Node("c")))
 begin_pynode_next(test)
